bcccf57ddf27bbb57541ee57d67a32c860aa6658
Chris <underaglassmoon27@gmail.com>
2018-02-26 14:49:22 -0800

    MVC Comic Book Challenge
    A simple MVC master/detail app that navigates to different pages based on the selected comic.

---
+++ VisualStudio/FirstChallenge/FirstChallenge/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using FirstChallenge.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstChallenge.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var getComics = ComicBookManager.GetComicBooks();
+
+
+            return View(getComics);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var comics = ComicBookManager.GetComicBooks();
+            var comic = comics.FirstOrDefault(p => p.ComicBookId == id);
+
+            return View(comic);
+        }
+    }
+}
